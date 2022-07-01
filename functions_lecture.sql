8994101a77f89509bfff7af0a0ccae43a26a3088
Marcus Munoz <munoz.marcus05@gmail.com>
2022-07-01 15:43:22 -0500

    list of functions

---
+++ functions_lecture.sql
@@ -0,0 +1,66 @@
+# Functions  Lecture
+use employees;
+
+
+# Aggregate functions
+# MAX() - returns the largest value in a set of values
+# MIN() - returns the smallest value in a set of values
+# SUM() - returns the sum of all values in a set of values
+# AVG() - returns the average value of all values in a set of values
+# COUNT() - returns the number of values in a set of values
+
+
+# Functions for date and time
+# DATE() - returns the current date
+# DATETIME() - returns the current date and time
+# DATE_ADD() - adds a specified number of days, months, or years to a date and time
+# DATEDIFF() - returns the number of days, months, or years between two date and times
+# DAY() - returns the day of the month for a date
+
+# HOUR() - returns the hour for a date
+# MINUTE() - returns the minute for a date
+
+
+# Functions for strings
+# CHAR() - returns the character at a specified position
+# CONCAT() - concatenates two strings
+# DIFFERENCE() - returns the difference between two strings
+# LEFT() - returns the leftmost characters of a string
+# LENGTH() - returns the length of a string
+# LOWER() - returns the string in lowercase
+# LTRIM() - removes whitespace from the beginning of a string
+# NCHAR() - returns the number of characters in a string
+# REPLACE() - replaces a part of a string with another string
+# RIGHT() - returns the rightmost characters of a string
+# RTRIM() - removes whitespace from the end of a string
+# SUBSTR() - returns a part of a string
+# TRIM() - removes whitespace from the beginning and end of a string
+# INSTR() - returns the position of the first occurrence of a substring
+# LEN() - returns the length of a string
+# LOWER() - returns the string in lowercase
+# UPPER() - returns the string in uppercase
+
+
+
+# Functions for numbers
+# ABS() - returns the absolute value of a number
+# CEILING() - returns the smallest integer not less than a number
+# COS() - returns the cosine of a number
+# COT() - returns the cotangent of a number
+# DEGREES() - returns the specified number in degrees
+# EXP() - returns e raised to the power of a number
+# FLOOR() - returns the largest integer not greater than a number
+# PI() - returns the value of pi
+# POWER() - returns the result of raising a number to a power
+# ROUND() - returns the rounded value of a number
+# SIGN() - returns the sign of a number
+# SQRT() - returns the square root of a number
+# TRUNCATE() - returns the integer part of a number
+# RAND() - returns a random number between 0 and 1
+# ROUND() - returns the rounded value of a number
+# FLOOR() - returns the largest integer not greater than a number
+
+
+
+
+# Examples of functions
